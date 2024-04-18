4accf0d66375a278ba317ce51133f4cc66c634f6
Kevinwijesooriya <kevinwijesooriya@gmail.com>
2024-04-18 22:57:52 +0530

    ui changes

--- frontend/src/Views/InventoryManagement/LoadManagement/LoadDetailList.js
+++ frontend/src/Views/InventoryManagement/LoadManagement/LoadDetailList.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { getAllLoadDetails } from '../../../services/InventoryManagementService/LoadDetailsService'; // Import the API function
-import { Stack, Typography } from "@mui/material";
+import { Divider, Stack, Typography } from "@mui/material";
 import {
   Grid,
   Table,
@@ -74,58 +74,95 @@ const LoadDetailList = () => {
           </Stack>
         </Grid>
         <Grid item xs={12}>
-          <TableContainer
-            component={Paper}
-            sx={{
-              bgcolor: "background.default",
-            }}
-          >
-            <Table>
-              <TableHead
-                sx={{
-                  bgcolor: Theme.palette.primary.mainBgS1,
-                }}
-              >
-                <TableRow>
-                  <TableCell>ID</TableCell>
-                  <TableCell>Seller Name</TableCell>
-                  <TableCell>Permit Number</TableCell>
-                  <TableCell>Region</TableCell>
-                  <TableCell>Lorry Number</TableCell>
-                  <TableCell>Driver</TableCell>
-                  <TableCell>Other Details</TableCell>
-                  <TableCell>Status</TableCell>
-                  <TableCell>Created By</TableCell>
-                  <TableCell>Modified By</TableCell>
-                  <TableCell>Actions</TableCell>
-                </TableRow>
-              </TableHead>
+          <TableContainer>
+            <Table size="small">
               <TableBody>
+                <TableRow>
+                  <TableCell sx={{ color: "primary.main", fontSize: "12px" }}>
+                    ID
+                  </TableCell>
+                  <TableCell sx={{ color: "primary.main", fontSize: "12px" }}>
+                    Seller Name
+                  </TableCell>
+                  <TableCell sx={{ color: "primary.main", fontSize: "12px" }}>
+                    Permit Number
+                  </TableCell>
+                  <TableCell sx={{ color: "primary.main", fontSize: "12px" }}>
+                    Region
+                  </TableCell>
+                  <TableCell sx={{ color: "primary.main", fontSize: "12px" }}>
+                    Lorry Number
+                  </TableCell>
+                  <TableCell sx={{ color: "primary.main", fontSize: "12px" }}>
+                    Driver
+                  </TableCell>
+                  <TableCell sx={{ color: "primary.main", fontSize: "12px" }}>
+                    Other Details
+                  </TableCell>
+                  <TableCell sx={{ color: "primary.main", fontSize: "12px" }}>
+                    Status
+                  </TableCell>
+                  <TableCell sx={{ color: "primary.main", fontSize: "12px" }}>
+                    Created By
+                  </TableCell>
+                  <TableCell sx={{ color: "primary.main", fontSize: "12px" }}>
+                    Modified By
+                  </TableCell>
+                  <TableCell sx={{ color: "primary.main", fontSize: "12px" }}>
+                    Actions
+                  </TableCell>
+                </TableRow>
+              </TableBody>
+              <TableBody
+              // sx={{
+              //   "& > :not(:last-child)": {
+              //     borderBottom: `6px solid #fff`,
+              //   },
+              // }}
+              >
                 {categories.map((category, index) => (
-                  <TableRow key={index}>
-                    <TableCell>{category.id}</TableCell>
-                    <TableCell>{category.sellerName}</TableCell>
-                    <TableCell>{category.permitNumber}</TableCell>
-                    <TableCell>{category.region}</TableCell>
-                    <TableCell>{category.lorryNumber}</TableCell>
-                    <TableCell>{category.driver}</TableCell>
-                    <TableCell>{category.otherDetails}</TableCell>
-                    <TableCell>{category.status}</TableCell>
-                    <TableCell>{category.createdBy}</TableCell>
-                    <TableCell>{category.modifiedBy}</TableCell>
-                    <TableCell>
-                      <Link>
-                        <Button
-                          variant="contained"
-                          component={Link}
-                          size="small"
-                          to={`/load/timber/view/${category.id}`}
-                        >
-                          View
-                        </Button>
-                      </Link>
-                    </TableCell>
-                  </TableRow>
+                  <>
+                    <TableRow
+                      key={index}
+                      sx={{
+                        bgcolor: "background.default",
+                      }}
+                    >
+                      {" "}
+                      <TableCell colSpan={11}></TableCell>
+                    </TableRow>
+                    <TableRow
+                      key={index}
+                      sx={{
+                        bgcolor: Theme.palette.primary.mainBgS1,
+                      }}
+                    >
+                      <TableCell sx={{ color: "primary.main" }}>
+                        {category.id}
+                      </TableCell>
+                      <TableCell>{category.sellerName}</TableCell>
+                      <TableCell>{category.permitNumber}</TableCell>
+                      <TableCell>{category.region}</TableCell>
+                      <TableCell>{category.lorryNumber}</TableCell>
+                      <TableCell>{category.driver}</TableCell>
+                      <TableCell>{category.otherDetails}</TableCell>
+                      <TableCell>{category.status}</TableCell>
+                      <TableCell>{category.createdBy}</TableCell>
+                      <TableCell>{category.modifiedBy}</TableCell>
+                      <TableCell>
+                        <Link>
+                          <Button
+                            variant="contained"
+                            component={Link}
+                            size="small"
+                            to={`/load/timber/view/${category.id}`}
+                          >
+                            View
+                          </Button>
+                        </Link>
+                      </TableCell>
+                    </TableRow>
+                  </>
                 ))}
               </TableBody>
             </Table>
