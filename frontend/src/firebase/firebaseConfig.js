f0072010966157b727d8c4be8eb463e87628df13
Aroshana0215 <aroshana@hatchyard.io>
2025-03-27 00:08:49 +0530

    changes up to 03/27

--- frontend/src/firebase/firebaseConfig.js
+++ frontend/src/firebase/firebaseConfig.js
@@ -4,15 +4,25 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage"; 
 
-// Your web app's Firebase configuration
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCrJEMG2srxm665yfJUJolYSiYX5Pj95vU",
+//   authDomain: "dts-client.firebaseapp.com",
+//   projectId: "dts-client",
+//   storageBucket: "dts-client.appspot.com",
+//   messagingSenderId: "880085947366", 
+//   appId: "1:880085947366:web:ee8010f84df7588733bc27",
+//   measurementId: "G-BBWE0Q93CP",
+// };
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCrJEMG2srxm665yfJUJolYSiYX5Pj95vU",
-  authDomain: "dts-client.firebaseapp.com",
-  projectId: "dts-client",
-  storageBucket: "dts-client.appspot.com",
-  messagingSenderId: "880085947366",
-  appId: "1:880085947366:web:ee8010f84df7588733bc27",
-  measurementId: "G-BBWE0Q93CP",
+  apiKey: "AIzaSyCOXiwyEFkDRzZRYeqaHaJsw4Sg2mR4Bs0",
+  authDomain: "dts-dev-287e5.firebaseapp.com",
+  projectId: "dts-dev-287e5",
+  storageBucket: "dts-dev-287e5.firebasestorage.app",
+  messagingSenderId: "494170016714",
+  appId: "1:494170016714:web:15167ca0c64d114dc95d33",
+  measurementId: "G-4J325DKHTC"
 };
 
 // Initialize Firebase
